8059c3b822c41c2f09ffc40f2c48d29a43c7348c
Rafael Arango <106997421+RarangoH@users.noreply.github.com>
2022-10-20 15:19:42 -0500

    Edited

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () =>{
+  const course={
+   name: 'Half Stack application development',
+ 
+  parts: [
+  {
+       name: 'Fundamentals of React ',
+       exercises: 10
+   },
+  
+  {
+       name: 'Using props to pass data',
+       exercises: 7
+  },
+ {
+       name:'State of a component',
+       exercises: 14
+  }
+  
+ ]
+ 
+ }
+  return(
+   <div>
+     <Header course = {course.name}/>
+     <Content parts ={course.parts}/>
+     <Total parts={course.parts}/>
+     
+   </div>
+ )
+ }
+ 
+ const Header = (props) => {
+   return(
+   <div>
+   <h1>
+     {props.course}
+   </h1>
+   </div>
+   )
+ }
+ const Content = (props) => {
+   
+   return (
+   <div>
+     
+     <Part p={props.parts}/>
+   </div>
+   )
+ }
+ const Part = (props) => {
+   return (
+     <div>
+       {props.p.map(one=><p key={one.name}>{one.name} {one.exercises}</p>)}
+       
+       
+     </div>
+   )
+ }
+ 
+ const Total = (props) => {
+ var suma = 0;
+   return(
+     <>
+     {/* {props.exercises1 + props.exercises2 + props.exercises3} */}
+       <p>Number of exercises {props.parts.forEach(element => {console.log(element.exercises)
+                                                                  suma = suma + element.exercises})} {suma} </p>
+     </>
+   )
+   
+ }
+ 
+ 
+ 
+ export default App;
+ 
