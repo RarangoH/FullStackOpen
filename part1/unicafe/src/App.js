f518ad3b0c3e4aecbae2e5176c01be19a0680cb9
Rafael Arango <106997421+RarangoH@users.noreply.github.com>
2022-10-20 16:43:39 -0500

    unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,6 +11,9 @@ const App = () => {
   const textG = 'good'
   const textN = 'neutral'
   const textB = 'bad'
+  var total = 0
+
+  
 
   const increaseGood = () => {
     setGood(good + 1)
@@ -23,6 +26,24 @@ const App = () => {
   const increaseBad = () => {
     setBad(bad + 1)
   }
+  const average = (g,n,b) => {
+    
+      if(g == 0 && b == 0 && n == 0){
+        return 0
+      }else{
+        total = (g-b)/(g+n+b)
+        return(total)
+      }
+  }
+
+  const percentage = (g,n,b) => {
+    if(g == 0 && b == 0 && n == 0){
+      return 0
+    }else{
+      return ((g/(g+n+b))*100)
+    }
+  }
+  
 
   return (
     <div>
@@ -34,7 +55,9 @@ const App = () => {
       <Count text={textG} count={good} />
       <Count text={textN} count={neutral} />
       <Count text={textB} count={bad} />
-
+      <Count text={'all'} count={good+bad+neutral}/>
+      <Count text ={'average'} count={average(good,neutral,bad)} />
+      <Count text = {'percentage'} count={percentage(good,neutral,bad)+'%'} />
     </div>
   )
 }
