8440f7a9c2b20428023153b5ce92da7576f7c639
Rafael Arango <106997421+RarangoH@users.noreply.github.com>
2022-10-31 07:18:39 -0500

    Revert "changes"
    This reverts commit 90fe01002fa5d3b5b1ce7c4b6b7774f9a437ba76.

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,18 +1,25 @@
-import Note from './components/Note'
+import logo from './logo.svg';
+import './App.css';
 
-import { useState } from 'react'
-
-const App = (props) => {  const [notes, setNotes] = useState(props.notes)
+function App() {
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App 
+export default App;
