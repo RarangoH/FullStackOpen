7034a39a642c89deed70cfe7598f159a95fcab01
Rafael Arango <106997421+RarangoH@users.noreply.github.com>
2022-10-31 21:10:15 -0500

    working on loading names with input letters

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,42 +1,72 @@
 import { useState } from 'react'
+import Persona from './components/Persona';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      phone: '393234209823041' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) ;
   const [newName, setNewName] = useState('');
-  const [newPhone, setPhone] = useState('');
+  const [newNumber, setNumber] = useState('');
+  const [newContNames, setContNames] = useState([]);
+
+ 
 
 
   const handleChangeName = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   }
-  const handleChangePhone = (event) => {
+  const handleChangeNumber = (event) => {
     console.log(event.target.value);
-    setPhone(event.target.value);
+    setNumber(event.target.value);
   }
+
+  
+  const handleChangeSearch =(event) => {
+    console.log(event.target.value);
+    persons.forEach(p => {
+      if(p.name.includes(event.target.value)){
+        console.log("objName",p.name)
+        const pObject={
+          name: p.name,
+          number: p.number,
+          id:p.id
+        }
+        setContNames(newContNames.concat(pObject));
+        
+      }
+      
+    })
+    // console.log("ola",newContNames)
+    
+    
+  }
+
 
   const addPerson = (event) =>{
     
     event.preventDefault();
     const personObject={
       name: newName,
-      phone: newPhone
+      number: newNumber,
+      id:persons[persons.length-1].id +1
     }
   
     console.log("name",personObject.name)
     if(!arrayVer(personObject)){
       setPersons(persons.concat(personObject));
       setNewName('');
-      setPhone('');
+      setNumber('');
     }else{
       alert(`${personObject.name} is already added to the phonebook`);
     }
     
     
   }
+ 
 
   const arrayVer = (x) =>{
     
@@ -48,14 +78,17 @@ const App = () => {
       return false;
     }
   }
+
   console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input onChange={handleChangeSearch}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         
       <div>name: <input value={newName} onChange={handleChangeName}/></div>
-      <div>number:<input value={newPhone} onChange={handleChangePhone}/></div>
+      <div>number:<input value={newNumber} onChange={handleChangeNumber}/></div>
       
           
         
@@ -64,9 +97,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div> {persons.map(p => <div key={p.name}> {p.name}</div>)}</div>
+      
+     <Persona persons={persons} />
+      {/* <div> {persons.map(p => <div key={p.id}> {p.name} {p.number}</div>)}</div> */}
     </div>
   )
 }
-
 export default App
---
+++ part2/phonebook/src/components/Persona.js
@@ -0,0 +1,19 @@
+
+
+
+
+const Persona = ({persons}) =>{
+   
+   
+    
+
+        return (
+            <div>
+                {persons.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
+            </div>
+        )
+    
+    
+}
+
+export default Persona
