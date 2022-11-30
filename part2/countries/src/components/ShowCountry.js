8bab95fdcbcbb684410951eb43f101decc9d4e76
Rafael Arango <106997421+RarangoH@users.noreply.github.com>
2022-11-30 09:57:39 -0500

    2.14

--- part2/countries/src/components/ShowCountry.js
+++ part2/countries/src/components/ShowCountry.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 const ShowCountry = ({showCountries}) => {
 
     const [weather, setWeather] = useState([]);
+    const [temp, setTemp] = useState('');
+    const [wind, setWind] = useState('');
+    const [icon, setIcon] = useState('');
 
     console.log("pais",showCountries);
 
@@ -18,16 +21,22 @@ const ShowCountry = ({showCountries}) => {
         .get(api)
         .then(response =>{
             const resp = response.data
-            console.log('promise fulfilled')
-            setWeather(resp)
-        
+            console.log('promise fulfilled');
+            setWeather(resp);
+            assignTemp(resp);
+            
         })
       },[])
 
       console.log("validity",weather);
-      console.log(Object.values(weather)[8].type);
+    //   console.log(weather.main.temp);
       
-    
+    const assignTemp = (param) =>{
+        
+        setTemp(param.main.temp - 273.15);
+        setWind(param.wind.speed);
+        setIcon("http://openweathermap.org/img/w/" + param.weather[0].icon + ".png")
+    }
       
 
     return(
@@ -41,8 +50,9 @@ const ShowCountry = ({showCountries}) => {
             {<ul>{Object.values(showCountries[0].languages).map(l => <li key={l}>{l}</li>)} </ul>} 
             <img alt='IMG' src={Object.values(showCountries[0].flags)[0]}></img> 
             <h2>Weather in {showCountries[0].capital}</h2>
-            <div>temperature  Celcius</div>
-
+            <div>temperature  Celcius {temp}</div>
+            <div id="icon"><img id="wicon" src={icon} alt="Weather icon"></img> </div>
+            <div>Wind {wind} m/s</div>
         </div>
     )
 }
